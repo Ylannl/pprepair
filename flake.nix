aa4f11597ac5ff7558437840b05a5f1c0afee812
ylannl <ravi@ylan.nl>
2026-05-17 15:58:05 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Planar partition repair";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "pprepair";
+            version = "0.1.0";
+            src = self;
+            nativeBuildInputs = with pkgs; [ cmake ];
+            buildInputs = with pkgs; [ cgal boost gdal ];
+          };
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ cmake cgal boost gdal ];
+          };
+        }
+      );
+    };
+}
